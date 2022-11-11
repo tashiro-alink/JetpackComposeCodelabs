d41134a71fe841152d33298ae51a35451bd65057
Ai Tashiro <tashiro@alink.ne.jp>
2022-11-11 11:26:25 +0900

    [1-6] 5. コンポーズ可能な関数のメモリ

--- BasicStateCodelab/app/src/main/java/com/codelab/basicstate/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelab/basicstate/WaterCounter.kt
@@ -5,13 +5,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        var count by remember {
+            mutableStateOf(0)
+        }
+
         Text(
             text = "You've had $count glasses."
         )
